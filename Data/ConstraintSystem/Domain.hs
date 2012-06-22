7fce5e55c4b5534b825994e7592c5d91e62c604c
Josh Burroughs <josh@qhool.com>
2012-06-22 00:35:10 -0400

    Split ModuloDomain into new Integer.hs Created SetDomain class for domains implemented with Data.Set

--- Data/ConstraintSystem/Domain.hs
+++ Data/ConstraintSystem/Domain.hs
@@ -1,13 +1,7 @@
 {-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies, FlexibleInstances #-}
-
 module Data.ConstraintSystem.Domain (Domain(..),
                                      FiniteDomain(..)
                                     ) where
-
-import Data.List
-import qualified Data.TypeLevel as Typ
-import Data.Set (Set)
-import qualified Data.Set as Set
 
 -- | Instances of Domain embody an unknown within a given domain.
 class Domain d a where
@@ -41,49 +35,3 @@ class (Domain d a) => FiniteDomain d a where
   -- | list all values
   elems :: d a -> [a]
 
-data ModuloDomain mod a = 
-  All | Empty | Listed { values :: Set a }
-
-
-
-modulus :: (Typ.Nat mod, Integral a) => ModuloDomain mod a -> a
-modulus x = Typ.toNum (gt x) where
-  gt :: (Typ.Nat mod) => ModuloDomain mod a -> mod
-  gt = undefined
-  
-applyModulus :: (Typ.Nat mod, Integral a) => 
-                ModuloDomain mod a -> 
-                ModuloDomain mod a
-applyModulus All = All
-applyModulus Empty = Empty
-applyModulus d@(Listed s) = Listed (Set.map (`mod` (modulus d)) s)
-
-instance (Typ.Nat mod, Integral a) => Domain (ModuloDomain mod) a where
-  universe = All
-  nothing = Empty
-  single x = applyModulus $ Listed (Set.singleton x)
-  enumerated xs = applyModulus $ Listed (Set.fromList xs)
-  All `subsumes` _ = True
-  _ `subsumes` Empty = True
-  _ `subsumes` All = False
-  Empty `subsumes` _ = False
-  (Listed s1) `subsumes` (Listed s2) = s2 `Set.isSubsetOf` s1
-  contains All x = True
-  contains Empty x = False
-  contains d@(Listed s) x = (x `mod` (modulus d)) `Set.member` s
-  reduce All d = d
-  reduce d All = d
-  reduce Empty _ = Empty
-  reduce _ Empty = Empty
-  reduce (Listed s1) (Listed s2) = let isect = Set.intersection s1 s2 in
-    if Set.null isect then Empty else Listed isect
-  expand All _ = All
-  expand _ All = All
-  expand Empty d = d
-  expand d Empty = d
-  expand (Listed s1) (Listed s2) = Listed (Set.union s1 s2)
-
-instance (Typ.Nat mod, Integral a) => FiniteDomain (ModuloDomain mod) a where
-  elems Empty = []
-  elems d@(All) = init [0..(modulus d)]
-  elems (Listed s1) = Set.toList s1
---
+++ Data/ConstraintSystem/Domain/Integer.hs
@@ -0,0 +1,74 @@
+{-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies, FlexibleInstances, UndecidableInstances #-}
+module Data.ConstraintSystem.Domain.Integer () where
+
+import Data.ConstraintSystem.Domain
+import Data.List
+import qualified Data.TypeLevel as Typ
+import Data.Set (Set)
+import qualified Data.Set as Set
+
+class SetDomain d a where
+  empty :: d a
+  complete :: d a
+  getSet :: d a -> Set a
+  fromSet :: Set a -> d a
+  isEmpty :: d a -> Bool
+  isComplete :: d a -> Bool
+  allowed :: d a -> a -> Bool
+  
+makeCanonical :: SetDomain d a => Set a -> d a
+makeCanonical s = if Set.null s then empty else fromSet s
+  
+filterElements :: (Ord a,SetDomain d a) => d a -> d a
+filterElements dom | isEmpty dom = empty
+                   | isComplete dom = complete
+                   | otherwise = makeCanonical $ Set.filter (allowed dom) $ getSet dom 
+
+instance (Ord a,SetDomain d a) => Domain d a where
+  universe = complete
+  nothing = empty
+  single x = filterElements $ fromSet (Set.singleton x)
+  enumerated xs = filterElements $ fromSet (Set.fromList xs)
+  subsumes a b | isComplete a = True
+               | isEmpty b = True
+               | isComplete b = False
+               | isEmpty a = False
+               | otherwise = Set.isSubsetOf (getSet b) (getSet a)
+  contains d x | not (allowed d x) = False
+               | isComplete d = True
+               | isEmpty d = False
+               | otherwise = x `Set.member` (getSet d)
+  reduce a b | isComplete a = b
+             | isComplete b = a
+             | (isEmpty a) || (isEmpty b) = empty
+             | otherwise = makeCanonical $ Set.intersection (getSet a) (getSet b)
+  expand a b | (isComplete a) || (isComplete b) = complete
+             | isEmpty a = b
+             | isEmpty b = a
+             | otherwise = makeCanonical $ Set.union (getSet a) (getSet b)
+                         
+data ModuloDomain mod a = 
+  All | Empty | Listed { values :: Set a }
+
+modulus :: (Typ.Nat mod, Integral a) => ModuloDomain mod a -> a
+modulus x = Typ.toNum (gt x) where
+  gt :: (Typ.Nat mod) => ModuloDomain mod a -> mod
+  gt = undefined
+  
+instance (Typ.Nat mod, Integral a) => SetDomain (ModuloDomain mod) a where
+  empty = Empty
+  complete = All
+  getSet Empty = Set.empty
+  getSet d@(All) = Set.fromList $ init [0..(modulus d)]
+  getSet (Listed s) = s
+  fromSet s = Listed s
+  isEmpty Empty = True
+  isEmpty _ = False
+  isComplete All = True
+  isComplete _ = False
+  allowed d x = (x >= 0) && (x < (modulus d))
+
+instance (Typ.Nat mod, Integral a) => FiniteDomain (ModuloDomain mod) a where
+  elems Empty = []
+  elems d@(All) = init [0..(modulus d)]
+  elems (Listed s1) = Set.toList s1
